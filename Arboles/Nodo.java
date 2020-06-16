4821b99770eadef5c55f220ea67cb712cc8449b4
Sofi Ramirez <62312917+SofiRM1809@users.noreply.github.com>
2020-06-16 00:54:25 -0500

    Add files via upload

---
+++ Arboles/Nodo.java
@@ -0,0 +1,24 @@
+package arboles;
+public class Nodo {
+    public Nodo Izq;
+    public Nodo Der;
+    private int Data;
+    
+    public Nodo (int Data){
+        this.Data = Data;
+        Izq = null;
+        Der = null;
+    }
+    
+    public int obtenerData(){
+        return Data;
+    }
+    
+    public void asignarData(int Data){
+        this.Data = Data;
+    }
+    
+    public String toString(){
+        return ""+Data;
+    }
+}
